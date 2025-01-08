1c261397d13287812e189852eb2f9296b2d7e256
Charlotte Dinsdale <charlottedins01@gmail.com>
2025-01-08 16:59:19 +0000

    re-ready for deployment

---
+++ staticfiles/js/script.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ) {
+    document.getElementById("castle-title").style.fontSize = "1.5rem";
+    document.getElementById("slogan").classList.add("hidden");
+  } else {
+    document.getElementById("castle-title").style.fontSize = "2.5rem";
+    document.getElementById("slogan").classList.remove("hidden");
+  }
+}
