b0f54be4d53659535d5798a1227e532fef224f57
Charlotte Dinsdale <charlottedins01@gmail.com>
2025-01-10 19:02:23 +0000

    add javascript to create carousel effect for testimonials

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -9,3 +9,40 @@ function scrollFunction() {
     document.getElementById("slogan").classList.remove("hidden");
   }
 }
+
+const testimonialCard = document.querySelectorAll('.testimonial-card');
+const prevButton = document.getElementById('prev-button');
+const nextButton = document.getElementById('next-button');
+
+let currentIndex = 0;
+
+function showTestimonial(index) {
+  testimonialCard.forEach((testimonialCard, i) => {
+    if (i === index) {
+      testimonialCard.style.display = 'block';
+    } else {
+      testimonialCard.style.display = 'none';
+    }
+  });
+}
+
+function showNextTestimonial() {
+  currentIndex++;
+  if (currentIndex >= testimonialCard.length) {
+    currentIndex = 0;
+  }
+  showTestimonial(currentIndex);
+}
+
+function showPreviousTestimonial() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = testimonialCard.length - 1;
+  }
+  showTestimonial(currentIndex);
+}
+
+prevButton.addEventListener('click', showPreviousTestimonial);
+nextButton.addEventListener('click', showNextTestimonial);
+
+showTestimonial(currentIndex);
